bbf2d9bbd9796fb3d6071ac35322791c0882b2f4
azizulwd <azizulislamgd@gmail.com>
2021-09-26 00:30:08 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header">
+                <div className="container">
+                    <div className="name">
+                        <h1>Top 10 University</h1>
+                        <p>Best top Bangladeshi University List</p>
+                    </div>
+                    <div className="budget">
+                        <h2>Total Budget: 50000 BDT</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
