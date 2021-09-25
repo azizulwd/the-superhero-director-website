b8a0f5a744a569f05d22158ba3f0a9231c385f20
azizulwd <azizulislamgd@gmail.com>
2021-09-26 04:55:20 +0600

    install fontawesome and added

--- src/components/University/University.js
+++ src/components/University/University.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './University.css';
 
 const University = (props) => {
     // console.log(props.university);
 
     const {name, image, city, position, Budget} = props.university;
+
+    const addIcon = <FontAwesomeIcon icon={faPlus} />
     return (
         <div className="single_university">
             <img src={image} alt="" />
@@ -14,7 +18,7 @@ const University = (props) => {
                 <p><span style={{fontWeight: 'bold'}}>Position: </span>{position} in Bangladesh</p>
                 <p><span style={{fontWeight: 'bold'}}>Budget: </span>{Budget}</p>
             </div>
-            <button onClick={()=> props.addedMemebrs(props.university)}>Add University</button>
+            <button onClick={()=> props.addedMemebrs(props.university)}>{addIcon} Add University</button>
         </div>
     );
 };
